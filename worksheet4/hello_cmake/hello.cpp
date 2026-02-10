784eb745cc915a9d1b73c8981fe34981a5f6678a
yuraaaa241 <eeyys3@nottingham.ac.uk>
2026-02-10 17:56:07 +0000

    Worksheet 4 Exercise 3 static calc

---
+++ worksheet4/hello_cmake/hello.cpp
@@ -0,0 +1,10 @@
+// begin - hello.cpp ----------
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello Planet Earth!" << std::endl;
+    return 0;
+}
+//end - hello.cpp ------------
